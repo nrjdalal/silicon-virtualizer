66bbfde663b2ab833e9724861bdec7da99470b95
Neeraj Dalal <nd941z@gmail.com>
2021-06-07 03:27:30 +0530

    Update install-qemu.sh

--- install-qemu.sh
+++ install-qemu.sh
@@ -3,7 +3,7 @@ xcode-select -p 1>/dev/null 2>/dev/null
 checkXcode=$?
 if [ $checkXcode != 0 ]; then
   echo
-  echo "Please install Xcode command line first tools using"
+  echo "Please install Xcode command line tools first using"
   echo "$(tput setaf 6)xcode-select --install$(tput sgr0)"
   echo
   exit 1
