96813bf68793c625621913b70502242d7dd44cd3
Neeraj Dalal <nd941z@gmail.com>
2021-06-06 20:56:45 +0530

    Update install-qemu.sh

--- install-qemu.sh
+++ install-qemu.sh
@@ -4,7 +4,7 @@ git clone https://github.com/qemu/qemu
 # Change directory to qemu repository
 cd qemu
 
-# Checkout to commit dated June 03, 2021 @ V6.0.0
+# Checkout to commit dated June 03, 2021 v6.0.0
 git checkout 3c93dfa42c394fdd55684f2fbf24cf2f39b97d47
 
 # Apply patch series v8 by Alexander Graf
