523ef0bc004552b576121936c4653e1cdcb99bd0
Neeraj Dalal <nd941z@gmail.com>
2021-06-06 20:56:04 +0530

    Update and rename qemu-installer.sh to install-qemu.sh

---
+++ install-qemu.sh
@@ -0,0 +1,19 @@
+# Clone qemu
+git clone https://github.com/qemu/qemu
+
+# Change directory to qemu repository
+cd qemu
+
+# Checkout to commit dated June 03, 2021 @ V6.0.0
+git checkout 3c93dfa42c394fdd55684f2fbf24cf2f39b97d47
+
+# Apply patch series v8 by Alexander Graf
+curl https://patchwork.kernel.org/series/485309/mbox/ | git am
+
+# Building qemu installer
+mkdir build && cd build
+../configure --target-list=aarch64-softmmu
+make -j8
+
+# Install qemu
+sudo make install
